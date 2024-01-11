3df9e1611b7cd432a1bf0c695577446ec18772b8
VictorArn <victor.arnautu14@gmail.com>
2024-01-12 01:30:25 +0200

    Forgot to add the APIs to the index

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import env from 'dotenv';
 import cors from "cors";
 import db_init from "./entities/db_init";
 import masterRouter from "./routes/masterRoute";
+import RequestRoute from "./routes/requestRoute";
 env.config();
 
 const app = express();
@@ -22,6 +23,8 @@ app.use(cors(corsOptions));
 db_init();
 
 app.use("/api", masterRouter);
+app.use("/api", RequestRoute);
+app.use("/api", StudentRoute);
 
 const port = process.env.PORT || 8001;
 app.listen(port);
